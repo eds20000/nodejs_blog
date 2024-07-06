069b1409a76d38cfc9a5858b671a6daef8f8702f
eds20000 <158028600+eds20000@users.noreply.github.com>
2024-07-06 20:04:26 +0900

    change

---
+++ src/routes/news.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const newsController = require('../app/controllers/NewController')
+
+//news Controller.index
+
+router.use('/:slug', newsController.show)
+
+router.use('/', newsController.index)
+
+
+module.exports = router
