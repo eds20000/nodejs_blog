089dc3428cc800560b66015196ea82cc25b28a80
eds20000 <158028600+eds20000@users.noreply.github.com>
2024-07-07 20:53:21 +0900

    changed husky

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const newsController = require('../app/controllers/NewController')
+const newsController = require('../app/controllers/NewController');
 
 //news Controller.index
 
-router.use('/:slug', newsController.show)
+router.use('/:slug', newsController.show);
 
-router.use('/', newsController.index)
+router.use('/', newsController.index);
 
-
-module.exports = router
+module.exports = router;
